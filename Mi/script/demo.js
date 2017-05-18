1432ab5d2a0ae215b2f18f99e59292d19cc5d16e
xiaohuihuihub <2209195052@qq.com>
2017-05-18 18:52:52 +0800

    r

--- Mi/script/demo.js
+++ Mi/script/demo.js
@@ -1,166 +1,217 @@
-var img=document.querySelectorAll(".ban li")
-var lun=document.querySelectorAll(".lunbo .dian")
-var banner=document.querySelector(".banner")
-var zuojian=document.querySelector(".zjs")
-var youjian=document.querySelector(".yjs")
-var obj=img[0]
-var obj1=lun[0]
-Array.from(lun).forEach(function (value,index) {
+var img = document.querySelectorAll(".ban li")
+var lun = document.querySelectorAll(".lunbo .dian")
+var banner = document.querySelector(".banner")
+var zuojian = document.querySelector(".zjs")
+var youjian = document.querySelector(".yjs")
+var obj = img[0]
+var obj1 = lun[0]
+Array.from(lun).forEach(function (value, index) {
 
-    value.onmouseover=function () {
-        num=index;
-        obj1.style.background="black"
-        obj.style.opacity=0;
-        obj.style.zIndex=1;
-        lun[index].style.background="white"
-        img[index].style.opacity=1;
-        img[index].style.zIndex=2;
-        obj1=lun[index];
-        obj=img[index];
+    value.onmouseover = function () {
+        num = index;
+        obj1.style.background = "black"
+        obj.style.opacity = 0;
+        obj.style.zIndex = 1;
+        lun[index].style.background = "white"
+        img[index].style.opacity = 1;
+        img[index].style.zIndex = 2;
+        obj1 = lun[index];
+        obj = img[index];
     }
 })
-var t=setInterval(move,2000);
-var num=0;
+var t = setInterval(move, 2000);
+var num = 0;
 function move() {
     num++;
-    if(num==lun.length){
-        num=0;
+    if (num == lun.length) {
+        num = 0;
     }
-    if(num==-1){
-        num=lun.length-1;
+    if (num == -1) {
+        num = lun.length - 1;
     }
-    obj1.style.background="black"
-    obj.style.zIndex=1;
-    obj.style.opacity=0;
-    lun[num].style.background="white"
-    img[num].style.zIndex=2;
-    img[num].style.opacity=1;
-    obj1=lun[num];
-    obj=img[num]
+    obj1.style.background = "black"
+    obj.style.zIndex = 1;
+    obj.style.opacity = 0;
+    lun[num].style.background = "white"
+    img[num].style.zIndex = 2;
+    img[num].style.opacity = 1;
+    obj1 = lun[num];
+    obj = img[num]
 }
 
-banner.onmouseover=function () {
+banner.onmouseover = function () {
     clearInterval(t)
 }
-banner.onmouseout=function () {
-    t=setInterval(move,2000)
+banner.onmouseout = function () {
+    t = setInterval(move, 2000)
 }
-youjian.onclick=function(){
+youjian.onclick = function () {
     move();
 }
-zuojian.onclick=function(){
-    num-=2
+zuojian.onclick = function () {
+    num -= 2
     move();
 }
 //购物车
-var gouwu=document.querySelector(".gouwu")
-var menu=document.querySelector(".menu")
-gouwu.onmouseover=function () {
-    menu.style.height=98+"px"
+var gouwu = document.querySelector(".gouwu")
+var menu = document.querySelector(".menu")
+gouwu.onmouseover = function () {
+    menu.style.height = 98 + "px"
 }
-gouwu.onmouseout=function () {
-    menu.style.height=0;
+gouwu.onmouseout = function () {
+    menu.style.height = 0;
 }
 //nav
-var clearfix=document.querySelectorAll(".clear .clearfix")
-var navli=document.querySelectorAll(".nav-right ul>.li1 a")
 // console.log(navli)
-Array.from(navli).forEach(function (v,i) {
-    v.onmouseover=function () {
-        clearfix[i].style.height=230+"px"
-        clearfix[i].style.borderTop="1px solid #000";
-    }
-    v.onmouseout=function () {
-        clearfix[i].style.height=0
-        clearfix[i].style.borderTop="0px solid #000";
-    }
+
+$(".nav-right ul>.li1").each(function (i,v) {
+    var isShow=false;
+    $(this).mouseover(function(){
+        $(".clearfix").eq(i).show();
+    });
+    $(this).mouseout(function () {
+        $(".clearfix").eq(i).hide()
+    })
+    $(".clearfix").eq(i).mouseover(function(){
+        // isShow=true;
+        $(this).show();
+    });
+    $(".clearfix").eq(i).mouseout(function(){
+
+            $(this).hide();
+            // isShow=false;
+       
+
+    });
 })
 //aside
-console.log($(".child").eq(2))
-$("aside>ul>li").each(function (i,v) {
+$("aside>ul>li").each(function (i, v) {
     $(this).hover(function () {
-      $(".child").eq(i).css("display","block")
-        console.log(i)
+        $(".child").eq(i).show()
+    }, function () {
+        $(".child").eq(i).hide()
+    })
+    $(".child").eq(i).hover(function () {
+        $(this).show()
     },function () {
-        $(".child").eq(i).css("display","none")
+        $(this).hide()
     })
 })
 //单品
-var danpin=setInterval(moves,5000)
-var dan=0;
+var danpin = setInterval(moves, 5000)
+var dan = 0;
 function moves() {
     // console.log(dan)
     dan++;
-    if(dan==1){
-        $("figure").css("marginLeft","-1240px")
-        $(".lunbo2-you").css("color","#ccc")
-        $(".lunbo2-zuo").css("color","#b0b0b0")
-        console.log(1);
+    if (dan == 1) {
+        $("figure").css("marginLeft", "-1240px")
+        $(".lunbo2-you").css("color", "#ccc")
+        $(".lunbo2-zuo").css("color", "#b0b0b0")
     }
-    else if(dan==2) {
-        console.log(2)
-        $("figure").css({marginLeft:0})
-        $(".lunbo2-you").css("color","#b0b0b0")
-        $(".lunbo2-zuo").css("color","#ccc")
-        dan=0;
+    else if (dan == 2) {
+        $("figure").css({marginLeft: 0})
+        $(".lunbo2-you").css("color", "#b0b0b0")
+        $(".lunbo2-zuo").css("color", "#ccc")
+        dan = 0;
     }
 }
 $("article").hover(function () {
     clearInterval(danpin)
-},function () {
-    danpin=setInterval(moves,5000)
+}, function () {
+    danpin = setInterval(moves, 5000)
 })
-var flagdan=true;
+var flagdan = true;
 $(".lunbo2-you").click(function () {
-        if(flagdan){
-            moves()
-        }
-        flagdan=false;
+    if (flagdan) {
+        moves()
+    }
+    flagdan = false;
 })
 $(".lunbo2-zuo").click(function () {
-        if(flagdan==false){
-            num=2;
-            moves()
-        }
-        flagdan=true;
+    if (flagdan == false) {
+        num = 2;
+        moves()
+    }
+    flagdan = true;
 })
 $(".lunbo2-you").hover(function () {
-    if(flagdan){
-        $(this).css("color","orange")
+    if (flagdan) {
+        $(this).css("color", "orange")
     }
-},function () {
-    if(flagdan){
-        $(this).css("color","")
+}, function () {
+    if (flagdan) {
+        $(this).css("color", "")
     }
 })
 $(".lunbo2-zuo").hover(function () {
-    if(flagdan==false){
-        $(this).css("color","orange")
+    if (flagdan == false) {
+        $(this).css("color", "orange")
     }
-},function () {
-    if(flagdan==false){
-        $(this).css("color","")
+}, function () {
+    if (flagdan == false) {
+        $(this).css("color", "")
     }
 })
-//热门家电
-var hh=5;
-$(".r").each(function (i,r) {
-    $(this).hover(function () {
-        hh++;
-       $(".ca").eq(i).css("z-index",hh)
-        $(".r").removeClass("actives").filter(this).addClass("actives")
+//切换
+function qiehuan(ea,ele) {
+    var hh = 5;
+    ea.each(function (i) {
+        $(this).hover(function () {
+            hh++;
+            ele.eq(i).css("z-index", hh)
+            ea.removeClass("actives").filter(this).addClass("actives")
+        })
     })
-})
-var tt=5;
-$(".da").each(function (i) {
-    console.log($(".da-he"))
+}
+qiehuan($(".r"),$(".ca"))
+qiehuan($(".da"),$(".da-he"))
+qiehuan($(".pj"),$(".pei"))
+qiehuan($(".zhou"),$(".zh"))
+//内容
+var flagn = true;
+function neirong(neli, qu, yk, zk,i) {
+    var flagku = true;
+    var mo;
+    var index;
+    neli.click(function () {
+        index = $(this).index();
+        li = index;
+        mo = index * 296;
+        neli.filter(".activess").removeClass("activess").end().eq(index).addClass("activess");
+        qu.css("marginLeft", -mo + "px");
+    }).hover(function () {
+        $(this).addClass("or")
+    }, function () {
+        $(this).removeClass("or")
+    })
+    var li = 0;
+    function mov() {
+        li++;
+        qu.css("marginLeft", -li * 296 + "px");
+    }
+    yk.click(function () {
+        if (li == 0 || li == 1||li==i) {
+            mov()
+            neli.filter(".activess").removeClass("activess").end().eq(li).addClass("activess");
+        }
 
-    $(this).hover(function () {
-        $(".da-he").eq(i).css("z-index",tt)
-        $(".da").removeClass("actives").filter(this).addClass("actives")
-        hh++;
     })
-})
+    zk.click(function () {
+        if (li==(i+1)||li == 2 || li == 1) {
+            li -= 2;
+            mov()
+            neli.filter(".activess").removeClass("activess").end().eq(li).addClass("activess");
+        }
+
+    })
+}
+
+neirong($(".neli1 .lun .lun1"), $(".neli1 .quan"), $(".neli1 .youkuang"), $(".neli1 .zuokuang"),1);
+neirong($(".neli2 .lun .lun1"), $(".neli2 .quan2"), $(".neli2 .youkuang"), $(".neli2 .zuokuang"),2);
+neirong($(".neli3 .lun .lun1"), $(".neli3 .quan3"), $(".neli3 .youkuang"), $(".neli3 .zuokuang"),2);
+neirong($(".neli4 .lun .lun1"), $(".neli4 .quan4"), $(".neli4 .youkuang"), $(".neli4 .zuokuang"),2);
+
+
 
 
 
